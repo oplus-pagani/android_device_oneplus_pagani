27fc309c3c80a3dd40a7de2cff54eb7e5b7f35df
Bruno Martins <bgcngm@gmail.com>
2026-02-15 18:15:45 +0000

    pagani: Drop surfaceflinger extension for UDFPS
    No longer necessary with OSS HWC.
    
    Change-Id: Ibd060431fb4ca7a1da16c6b9a3f9903ec701af74

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946640660707475.xml
 
 # Fingerprint
-$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 $(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
 # LiveDisplay
