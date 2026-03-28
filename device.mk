7526cbb7041bbcf6f2908ceb7e958450b4f096ab
KagehiraNE <ne@kagehira.icu>
2026-03-28 17:34:27 +1000

    pagani: Configure eUICC support
    Add the device-specific framework overlay, permission declaration, and proprietary EuiccGoogle components.

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    FrameworksResTargetEuicc \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
@@ -51,6 +52,14 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Telephony
+#PRODUCT_PACKAGES += \
+#    OplusEsimSwitcher \
+#    OplusEuicc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # Touch features
 $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
